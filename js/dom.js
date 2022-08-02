d1b0f8ae0a1271efd39a3b3ce89b689373d8c995
ysalem-dev-89 <yazeed.salem.it@gmail.com>
2022-08-02 21:03:31 +0300

    finished getting ayahs list of the selected surah Relates #14

--- js/dom.js
+++ js/dom.js
@@ -1,36 +1,36 @@
-// (function () {
-let surah = document.getElementById("surah");
-let ayah = document.getElementById("ayah");
-
-fetch("https://api.alquran.cloud/v1/surah", handleSurahs);
-fetch("https://api.alquran.cloud/v1/surah/1/edition", handleAyas);
-// })();
-
-function handleSurahs(response) {
-  response.data.forEach((ele) => {
+const url = "https://api.alquran.cloud/v1/surah";
+fetch(url, (res) => {
+  const selectSurah = document.querySelector("#surah");
+  for (let i = 0; i < res.data.length; i++) {
     let option = document.createElement("option");
-    option.value = ele.number;
-    option.textContent = `${ele.number} ${ele.englishName}`;
-    surah.appendChild(option);
+    option.setAttribute("value", `${res.data[i].number}`);
+    option.textContent = res.data[i].englishName;
+    selectSurah.appendChild(option);
 
-    option.onselect = (event) => {
-      fetch(`https://api.alquran.cloud/v1/surah/${event.target.value}/edition`, handleAyas);
+    selectSurah.onchange = (event) => {
+      let selectAyas = document.getElementById("ayah");
+      selectAyas.setAttribute("disabled", "disabled");
+      event.target.setAttribute("disabled", "disabled");
+      fetch(
+        `https://api.alquran.cloud/v1/surah/${selectSurah.options[selectSurah.selectedIndex].value}/edition`,
+        (res) => {
+          if (res.data["ayahs"].length) {
+            let ayasList = res["data"]["ayahs"];
+
+            while (selectAyas.firstChild) {
+              selectAyas.removeChild(selectAyas.lastChild);
+            }
+            for (let i = 1; i < ayasList.length; i++) {
+              let option = document.createElement("option");
+              option.setAttribute("value", ayasList[i]["number"]);
+              option.textContent = i;
+              selectAyas.appendChild(option);
+            }
+          }
+          selectAyas.removeAttribute("disabled");
+          event.target.removeAttribute("disabled");
+        }
+      );
     };
-  });
-}
-
-function handleAyas(response) {
-  let options = [];
-  response.data["ayahs"].forEach((ele) => {
-    let option = document.createElement("option");
-    option.value = ele.number;
-    option.textContent = ele.number;
-    options.push(option);
-  });
-  ayah.replaceChildren(options);
-}
-
-function renderAyas(surah) {
-  fetch("https://api.alquran.cloud/v1/surah/2/edition");
-  console.log();
-}
+  }
+});
