8bc99faf46a8318907b1a419e2b003652a52c2d5
Bakeza <dbakeza2002@gmail.com>
2022-08-02 19:45:50 +0300

    get surah values from api and show them in the drop down menu relate #12

--- js/dom.js
+++ js/dom.js
@@ -1,32 +1,45 @@
-(function () {
-  let surah = document.getElementById("surah");
-  let ayah = document.getElementById("ayah");
+// (function () {
+//   let surah = document.getElementById("surah");
+//   let ayah = document.getElementById("ayah");
 
-  fetch("https://api.alquran.cloud/v1/surah", handleSurahs);
-  fetch("https://api.alquran.cloud/v1/surah/2/edition", handleAyas);
-})();
+//   fetch("https://api.alquran.cloud/v1/surah", handleSurahs);
+//   fetch("https://api.alquran.cloud/v1/surah/2/edition", handleAyas);
+// })();
 
-function handleSurahs(response) {
-  response.data.forEach((surah) => {
+// function handleSurahs(response) {
+//   response.data.forEach((surah) => {
+//     let option = document.createElement("option");
+//     option.value = surah.number;
+//     option.textContent = `${surah.number} ${surah.englishName}`;
+//     surah.appendChild(option);
+
+//     option.onselect = (event) => {};
+//   });
+// }
+
+// function handleAyas(response) {
+//   response.data["ayahs"].forEach((ayah) => {
+//     let option = document.createElement("option");
+//     option.value = ayah.number;
+//     option.textContent = ayah.number;
+//     ayah.appendChild(option);
+//   });
+// }
+
+// function renderAyas(surah) {
+//   fetch("https://api.alquran.cloud/v1/surah/2/edition");
+//   console.log();
+// }
+
+const url = "https://api.alquran.cloud/v1/surah";
+fetch(url, (res) => {
+  const selectSurah = document.querySelector("#surah");
+  for (let i = 0; i < res.data.length; i++) {
     let option = document.createElement("option");
-    option.value = surah.number;
-    option.textContent = `${surah.number} ${surah.englishName}`;
-    surah.appendChild(option);
+    option.setAttribute("id", `${i}`)
+    option.textContent = res.data[i].englishName;
+    selectSurah.appendChild(option);
+    console.log(res.data[i]);
+  }
+});
 
-    option.onselect = (event) => {};
-  });
-}
-
-function handleAyas(response) {
-  response.data["ayahs"].forEach((ayah) => {
-    let option = document.createElement("option");
-    option.value = ayah.number;
-    option.textContent = ayah.number;
-    ayah.appendChild(option);
-  });
-}
-
-function renderAyas(surah) {
-  fetch("https://api.alquran.cloud/v1/surah/2/edition");
-  console.log();
-}
