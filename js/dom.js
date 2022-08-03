3c96fdf883ab58446020b2485852cc0d890fda1c
ysalem-dev-89 <yazeed.salem.it@gmail.com>
2022-08-03 15:31:48 +0300

    finished refactoring code - add loading spinner Relates #27

--- js/dom.js
+++ js/dom.js
@@ -3,11 +3,12 @@ const selectReciters = document.getElementById("reciter");
 const selectAyas = document.getElementById("ayah");
 const surahDiv = document.getElementById("showSurah");
 const soundPlayer = document.querySelector(".soundPlayer");
-let urlName = ""
+const loadingImage = document.querySelector(".loading");
+let fileUrl = "";
 
+fetch("https://api.alquran.cloud/v1/surah", renderSurahs);
 
-const url = "https://api.alquran.cloud/v1/surah";
-fetch(url, (res) => {
+function renderSurahs(res) {
   for (let i = 0; i < res.data.length; i++) {
     let option = document.createElement("option");
     option.setAttribute("value", `${res.data[i].number}`);
@@ -16,69 +17,72 @@ fetch(url, (res) => {
 
     selectSurahElem.onchange = (event) => {
       let surahId = selectSurahElem.options[selectSurahElem.selectedIndex].value;
-      let reciterId = selectReciters.options[event.target.selectedIndex].value;
-
       selectAyas.setAttribute("disabled", "disabled");
       event.target.setAttribute("disabled", "disabled");
-      fetch(`https://api.alquran.cloud/v1/surah/${surahId}/edition`, (res) => {
-        if (res.data["ayahs"].length) {
-          let ayasList = res["data"]["ayahs"];
+      selectReciters.setAttribute("disabled", "disabled");
 
-          while (selectAyas.firstChild) {
-            selectAyas.removeChild(selectAyas.lastChild);
-          }
-          for (let i = 1; i < ayasList.length; i++) {
-            let option = document.createElement("option");
-            option.setAttribute("value", ayasList[i]["number"]);
-            option.textContent = i;
-            selectAyas.appendChild(option);
-          }
-        }
-        selectAyas.removeAttribute("disabled");
-        event.target.removeAttribute("disabled");
-        renderAyas(res);
-      });
+      while (surahDiv.firstChild) {
+        surahDiv.removeChild(surahDiv.lastChild);
+      }
+      loadingImage.style.display = "block";
+
+      fetch(`https://api.alquran.cloud/v1/surah/${surahId}/edition`, handleAyahs);
     };
   }
-});
+}
+
+function handleAyahs(res) {
+  while (selectAyas.firstChild) {
+    selectAyas.removeChild(selectAyas.lastChild);
+  }
+
+  if (res.data["ayahs"].length) {
+    let ayasList = res["data"]["ayahs"];
+
+    for (let i = 1; i < ayasList.length; i++) {
+      let option = document.createElement("option");
+      option.setAttribute("value", ayasList[i]["number"]);
+      option.textContent = i;
+      selectAyas.appendChild(option);
+    }
+  }
+  selectAyas.removeAttribute("disabled");
+  selectSurahElem.removeAttribute("disabled");
+  selectReciters.removeAttribute("disabled", "disabled");
+
+  renderAyas(res);
+}
 
 // Get Recipters
-const recitersUrl = "https://qurani-api.herokuapp.com/api/reciters/";
-fetch(recitersUrl, (res) => {
+fetch("https://qurani-api.herokuapp.com/api/reciters/", (res) => {
   for (let i = 0; i < res.length; i++) {
-    console.log(res[i].id + " - " + res[i].name + " - " + res[i].rewaya);
-    console.log(res[i].name + " - " + res[i].rewaya);
-    console.log(getSoundUrl(res, 1, 1));
     let option = document.createElement("option");
     option.setAttribute("value", res[i].id);
     option.textContent = `${res[i].name} - ${res[i].rewaya}`;
     selectReciters.appendChild(option);
+
     selectReciters.onchange = (event) => {
       let surahOption = selectSurahElem.options[selectSurahElem.selectedIndex];
 
-      urlName = getSoundUrl(
-        res,
-        event.target.options[event.target.selectedIndex].value,
-        surahOption.value
-
-      );
-      soundPlayer.src = urlName;
-      document.querySelector('.playr-filename').innerHTML = `<span>${selectSurahElem.options[event.target.selectedIndex].textContent} - بصوت القارئ الشيخ ${selectReciters.options[selectReciters.selectedIndex].textContent}</span>`;
-
+      fileUrl = getSoundUrl(res, event.target.options[event.target.selectedIndex].value, surahOption.value);
+      soundPlayer.src = fileUrl;
+      document.querySelector(".playr-filename").innerHTML = `<span>${
+        selectSurahElem.options[event.target.selectedIndex].textContent
+      } - بصوت القارئ الشيخ ${selectReciters.options[selectReciters.selectedIndex].textContent}</span>`;
+      window.player.play();
     };
     selectSurahElem.addEventListener("change", (event) => {
-      console.log(
-
-      );
-      urlName = getSoundUrl(
+      fileUrl = getSoundUrl(
         res,
         selectReciters.options[selectReciters.selectedIndex].value,
         event.target.options[event.target.selectedIndex].value
-
       );
-      soundPlayer.src = urlName;
-      document.querySelector('.playr-filename').innerHTML = `<span>${selectSurahElem.options[event.target.selectedIndex].textContent} - بصوت القارئ الشيخ ${selectReciters.options[selectReciters.selectedIndex].textContent}</span>`;
-
+      soundPlayer.src = fileUrl;
+      soundPlayer;
+      document.querySelector(".playr-filename").innerHTML = `<span>${
+        selectSurahElem.options[event.target.selectedIndex].textContent
+      } - بصوت القارئ الشيخ ${selectReciters.options[selectReciters.selectedIndex].textContent}</span>`;
+      window.player.play();
     });
   }
 });
--- js/surah.js
+++ js/surah.js
@@ -1,8 +1,6 @@
 function renderAyas(res) {
   const surahDiv = document.getElementById("showSurah");
-  while (surahDiv.firstChild) {
-    surahDiv.removeChild(surahDiv.lastChild);
-  }
+
   const pars = res["data"]["ayahs"];
   for (let i = 0; i < pars.length; i++) {
     const paragraph = document.createElement("p");
@@ -12,4 +10,5 @@ function renderAyas(res) {
     surahDiv.appendChild(paragraph);
     surahDiv.appendChild(line);
   }
+  loadingImage.style.display = "none";
 }
